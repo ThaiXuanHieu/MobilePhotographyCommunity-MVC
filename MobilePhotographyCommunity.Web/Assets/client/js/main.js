3ac142e1d762ac4000af988b6b84e0659587ae21
ThaiXuanHieu <s2freedom6789@gmail.com>
2020-10-05 17:00:31 +0700

    "update ui"

--- MobilePhotographyCommunity.Web/Assets/client/js/main.js
+++ MobilePhotographyCommunity.Web/Assets/client/js/main.js
@@ -31,3 +31,23 @@ function closeDescription() {
     colCenter.style.marginTop = '70px';
 
 }
+
+const btnScroll = document.getElementById("btnScroll");
+
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        btnScroll.style.display = "block";
+    } else {
+        btnScroll.style.display = "none";
+    }
+}
+
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    window.location.reload();
+}
